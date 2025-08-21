cb0304b1daf42436a55d192e7018e8fd9d8f9b76
rutviranpariya <rutviranpariya.ce@gmail.com>
2025-08-21 22:46:39 +0530

    Add files via upload

---
+++ practical12/practical 12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=50;i++)
+    {
+        if(i%5==0)
+        {
+            printf("Book ID:%d(special edition)\n",i);
+
+        }
+        else
+        {
+
+
+            printf("Book ID:%d\n",i);
+    }
+
+    }
+    return 0;
+}
